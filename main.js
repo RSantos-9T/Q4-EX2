79de4e30c64d1441989761723be07ce249aae0d7
TheLockheedF35J <santos.rr@obmontessori.edu.ph>
2025-05-06 10:24:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function verifyAge(){
+    let age = document.getElementById("age").value;
+    if (age < 18) {window.alert("Sorry, you're too young to fill out the form. But not for sean's type.");} else{window.alert("You can now vote.")}
+}
+    
+   
